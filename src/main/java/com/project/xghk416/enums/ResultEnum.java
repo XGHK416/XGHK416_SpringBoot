f53eeee140c3858f1a767328c4b559b6fc863258
XGHK416 <1027868014@qq.com>
2019-12-20 21:17:08 +0800

    Jenkins测试2

--- src/main/java/com/project/xghk416/enums/ResultEnum.java
+++ src/main/java/com/project/xghk416/enums/ResultEnum.java
@@ -3,7 +3,7 @@ package com.project.xghk416.enums;
 public enum  ResultEnum {
     //这里是可以自己定义的，方便与前端交互即可
     UNKNOWN_ERROR(-1,"未知错误"),
-    SUCCESS(10000,"成功"),
+    SUCCESS(10000,"成功啦娃哈哈！！！"),
     USER_NOT_EXIST(1,"用户不存在"),
     USER_IS_EXISTS(2,"用户已存在"),
     DATA_IS_NULL(3,"数据为空"),
